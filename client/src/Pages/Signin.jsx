8cf561b8458951e0c7effe4a9dc6e2da13fcb13d
khadidja2005 <mk_brakta@esi.dz>
2024-02-02 19:09:42 +0100

    fixed some validation test

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
 
 const Signin = () => {
+  const [formData , SetformData] =useState({});
+  const handelChange = (e)=> { 
+    SetformData({...formData , [e.target.id] : e.target.value})
+  }
+  const handelSubmit =async (e) => {
+    e.preventDefault();
+    const res = await fetch('api/auth/signin',
+    {
+      
+      method: 'POST',
+      headers : {
+        'Content-Type': 'application/json' ,
+      },
+      body : JSON.stringify(formData),
+    }
+    )
+  const data = await res.json();
+  console.log(data);
+  }
+   console.log(formData)
   return (
-    <div>Signin</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className=' text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form className='flex flex-col gap-4' onSubmit={handelSubmit}>
+        <input type="email" name="" id="email" placeholder='email' className=' border rounded-lg p-3'onChange={handelChange} />
+        <input type="password" name="" id="password" placeholder='password' className=' border rounded-lg p-3'onChange={handelChange} />
+        <button className=' uppercase bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80'>Sign In</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>create a new account? </p>
+        <Link to={"/signup"} >
+          <span className=' text-blue-700'>  Sign up</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import {Link , useNavigate } from 'react-router-dom'
 
 const Signup = () => {
+  const [ test , settest] = useState(false);
   const [formData , SetformData] =useState({});
+  const [error , seterror] = useState(null);
+  const [loading , setloading] = useState(false);
   const handelChange = (e)=> { 
+    
     SetformData({...formData , [e.target.id] : e.target.value})
   }
   const handelSubmit =async (e) => {
+     
     e.preventDefault();
+    try {
+      setloading(true);
     const res = await fetch('api/auth/signup',
+
     {
       
       method: 'POST',
@@ -19,7 +27,24 @@ const Signup = () => {
     }
     )
   const data = await res.json();
+  if(data.success === false) {
+    seterror(data.message);
+    setloading(false)
+    
+    return
+  }
+ setloading(false);
+ seterror(null);
+  settest(true);
+ //{ true &&<p className=' text-green-500 mt-5'>user created successfully</p>}
+ //const navigate = useNavigate();
+ //useNavigate('/signin');
   console.log(data);
+    } catch (error) {
+     setloading(false);
+     seterror(error.message) 
+    }
+
   }
    console.log(formData)
   return (
@@ -29,7 +54,7 @@ const Signup = () => {
         <input type="text" name="" id="username" placeholder='username' className=' border rounded-lg p-3'onChange={handelChange} />
         <input type="email" name="" id="email" placeholder='email' className=' border rounded-lg p-3'onChange={handelChange} />
         <input type="password" name="" id="password" placeholder='password' className=' border rounded-lg p-3'onChange={handelChange} />
-        <button className=' uppercase bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+        <button disabled={loading} className=' uppercase bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-60'>{loading ? "loading ..." : "Sign Up"  }</button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Already have an account? </p>
@@ -37,6 +62,8 @@ const Signup = () => {
           <span className=' text-blue-700'>  Sign in</span>
         </Link>
       </div>
+      { test && <p className=' text-green-500 mt-5'>user created  successfully</p>}
+      {error && <p className=' text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
