d18052f5ecbf5ab0ac9b33bb0437c64a265613d3
khadidja2005 <mk_brakta@esi.dz>
2024-02-12 12:00:17 +0100

    profile ui

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
 
 const Profile = () => {
+  const {currentUser} = useSelector(stata => (state.user))
   return (
-    <div>Profile</div>
+    <div>
+      <h1 className=' text-3xl font-semibold text-center'>Profile</h1>
+      
+    </div>
   )
 }
 
